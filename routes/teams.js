49211c6b549c8a32b81c5094f8cd6a83af3e5145
Gouda <gouda.clark@gmail.com>
2018-09-20 11:04:27 -0400

    added routes for show all teams and show team

--- routes/teams.js
+++ routes/teams.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { Team } = require('../db/schema')
+
+//INDEX, SHOW ALL
+router.get('/', (req, res) => {
+  Team.find()
+  .then((teams) => {
+    res.render('teams/index', {teams})
+  })
+})
+
+//NEW, RENDER NEW FORM
+
+//SHOW,SHOW ONE
+router.get('/:id', (req,res) => {
+  Team.findById(req.params.id)
+  .then((team) => {
+    res.render('teams/show', { team })
+  })
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
