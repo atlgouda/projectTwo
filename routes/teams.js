ae771c019994be69b0e6e6d2d2cd8f68de492be2
Gouda <gouda.clark@gmail.com>
2018-09-21 10:07:41 -0400

    added CRUD routes to teams

--- routes/teams.js
+++ routes/teams.js
@@ -11,12 +11,43 @@ router.get('/', (req, res) => {
 })
 
 //NEW, RENDER NEW FORM
+router.get('/new', (req, res) => {
+  res.render('teams/new')
+})
 
 //SHOW,SHOW ONE
 router.get('/:id', (req,res) => {
   Team.findById(req.params.id)
   .then((team) => {
     res.render('teams/show', { team })
+  })
+})
+//EDIT, RENDER EDIT FORM
+router.get('/:id/edit', (req, res) => {
+  Team.findById(req.params.id)
+  .then((team) => {
+    res.render('teams/edit', { team })
+  })
+})
+//CREATE
+router.post('/', (req, res) => {
+  Team.create(req.body)
+  .then((team) => {
+    res.redirect(`/teams/${team._id}`)
+  })
+})
+//UPDATE
+router.put('/:id', (req, res) => {
+  Team.findByIdAndUpdate(req.params.id, req.body)
+  .then((team) => {
+    res.redirect(`/teams/${team._id}`)
+  })
+})
+//DELETE
+router.delete(':/id', (req, res) => {
+  Team.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.redirect('/teams')
   })
 })
 
